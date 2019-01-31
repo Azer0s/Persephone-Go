a7e07ceb20b5384cf53a1a4138a2bfb91218b0b1
Ariel Simulevski <ariel.simulevski@gmail.com>
2019-01-31 20:15:57 +0100

    Added most of the missing byte code compiler

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -2,101 +2,105 @@ package compiler
 
 import (
 	"../types"
+	"encoding/binary"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var opcodes = map[string]uint16{
-	"v_int8"	: uint16(0x0118),
-	"v_int16"	: uint16(0x0110),
-	"v_int32"	: uint16(0x0120),
-	"v_int"		: uint16(0x0120),
-	"v_int64"	: uint16(0x0140),
-	"v_float32"	: uint16(0x0121),
-	"v_float"	: uint16(0x0121),
-	"v_float64"	: uint16(0x0141),
-	"v_double"	: uint16(0x0141),
-	"v_stringa"	: uint16(0x0131),
-	"v_stringu"	: uint16(0x0132),
-	"v_ptr"		: uint16(0x0150),
-	"v_bit"		: uint16(0x0100),
-	"dci8"		: uint16(0x0218),
-	"dci16"		: uint16(0x0210),
-	"dci32"		: uint16(0x0220),
-	"dci"		: uint16(0x0220),
-	"dci64"		: uint16(0x0240),
-	"dcf32"		: uint16(0x0221),
-	"dcf"		: uint16(0x0221),
-	"dcf64"		: uint16(0x0241),
-	"dcd"		: uint16(0x0241),
-	"dcsa"		: uint16(0x0231),
-	"dcsu"		: uint16(0x0232),
-	"dcb"		: uint16(0x0200),
-	"ldi8v"		: uint16(0x0318),
-	"ldi16v"	: uint16(0x0310),
-	"ldi32v"	: uint16(0x0320),
-	"ldiv"		: uint16(0x0320),
-	"ldi64v"	: uint16(0x0340),
-	"ldf32v"	: uint16(0x0321),
-	"ldfv"		: uint16(0x0321),
-	"ldf64v"	: uint16(0x0341),
-	"lddv"		: uint16(0x0341),
-	"ldsav"		: uint16(0x0331),
-	"ldsuv"		: uint16(0x0332),
-	"ldptrv"	: uint16(0x0350),
-	"ldbv"		: uint16(0x0300),
-	"ldi8c"		: uint16(0x0418),
-	"ldi16c"	: uint16(0x0410),
-	"ldi32c"	: uint16(0x0420),
-	"ldic"		: uint16(0x0420),
-	"ldi64c"	: uint16(0x0440),
-	"ldf32c"	: uint16(0x0421),
-	"ldfc"		: uint16(0x0421),
-	"ldf64c"	: uint16(0x0441),
-	"lddc"		: uint16(0x0441),
-	"ldsac"		: uint16(0x0431),
-	"ldsuc"		: uint16(0x0432),
-	"ldbc"		: uint16(0x0400),
-	"pop"		: uint16(0x0001),
-	"ret"		: uint16(0x0002),
-	"add"		: uint16(0x0003),
-	"sub"		: uint16(0x0004),
-	"mul"		: uint16(0x0005),
-	"div"		: uint16(0x0006),
-	"mod"		: uint16(0x0007),
-	"ge"		: uint16(0x0008),
-	"le"		: uint16(0x0009),
-	"gt"		: uint16(0x000A),
-	"lt"		: uint16(0x000B),
-	"andi"		: uint16(0x000C),
-	"ori"		: uint16(0x000D),
-	"xori"		: uint16(0x000E),
-	"noti"		: uint16(0x000F),
-	"shl"		: uint16(0x0010),
-	"shr"		: uint16(0x0011),
-	"inc"		: uint16(0x0012),
-	"dec"		: uint16(0x0013),
-	"addf"		: uint16(0x0014),
-	"subf"		: uint16(0x0015),
-	"mulf"		: uint16(0x0016),
-	"divf"		: uint16(0x0017),
-	"gef"		: uint16(0x0018),
-	"lef"		: uint16(0x0019),
-	"gtf"		: uint16(0x001A),
-	"ltf"		: uint16(0x001B),
-	"and"		: uint16(0x001C),
-	"or"		: uint16(0x001D),
-	"xor"		: uint16(0x001E),
-	"not"		: uint16(0x001F),
-	"conc"		: uint16(0x0020),
-	"len"		: uint16(0x0021),
-	"getc"		: uint16(0x0022),
-	"setc"		: uint16(0x0023),
-	"syscall"	: uint16(0x0024),
-	"extern"	: uint16(0x0025),
-	"call"		: uint16(0xF000),
-	"jmp"		: uint16(0xF001),
-	"jmpt"		: uint16(0xF002),
-	"jmpf"		: uint16(0xF003),
+	"v_int8":    uint16(0x0118),
+	"v_int16":   uint16(0x0110),
+	"v_int32":   uint16(0x0120),
+	"v_int":     uint16(0x0120),
+	"v_int64":   uint16(0x0140),
+	"v_float32": uint16(0x0121),
+	"v_float":   uint16(0x0121),
+	"v_float64": uint16(0x0141),
+	"v_double":  uint16(0x0141),
+	"v_stringa": uint16(0x0131),
+	"v_stringu": uint16(0x0132),
+	"v_ptr":     uint16(0x0150),
+	"v_bit":     uint16(0x0100),
+	"dci8":      uint16(0x0218),
+	"dci16":     uint16(0x0210),
+	"dci32":     uint16(0x0220),
+	"dci":       uint16(0x0220),
+	"dci64":     uint16(0x0240),
+	"dcf32":     uint16(0x0221),
+	"dcf":       uint16(0x0221),
+	"dcf64":     uint16(0x0241),
+	"dcd":       uint16(0x0241),
+	"dcsa":      uint16(0x0231),
+	"dcsu":      uint16(0x0232),
+	"dcb":       uint16(0x0200),
+	"ldi8v":     uint16(0x0318),
+	"ldi16v":    uint16(0x0310),
+	"ldi32v":    uint16(0x0320),
+	"ldiv":      uint16(0x0320),
+	"ldi64v":    uint16(0x0340),
+	"ldf32v":    uint16(0x0321),
+	"ldfv":      uint16(0x0321),
+	"ldf64v":    uint16(0x0341),
+	"lddv":      uint16(0x0341),
+	"ldsav":     uint16(0x0331),
+	"ldsuv":     uint16(0x0332),
+	"ldptrv":    uint16(0x0350),
+	"ldbv":      uint16(0x0300),
+	"ldi8c":     uint16(0x0418),
+	"ldi16c":    uint16(0x0410),
+	"ldi32c":    uint16(0x0420),
+	"ldic":      uint16(0x0420),
+	"ldi64c":    uint16(0x0440),
+	"ldf32c":    uint16(0x0421),
+	"ldfc":      uint16(0x0421),
+	"ldf64c":    uint16(0x0441),
+	"lddc":      uint16(0x0441),
+	"ldsac":     uint16(0x0431),
+	"ldsuc":     uint16(0x0432),
+	"ldbc":      uint16(0x0400),
+	"pop":       uint16(0x0001),
+	"ret":       uint16(0x0002),
+	"add":       uint16(0x0003),
+	"sub":       uint16(0x0004),
+	"mul":       uint16(0x0005),
+	"div":       uint16(0x0006),
+	"mod":       uint16(0x0007),
+	"ge":        uint16(0x0008),
+	"le":        uint16(0x0009),
+	"gt":        uint16(0x000A),
+	"lt":        uint16(0x000B),
+	"andi":      uint16(0x000C),
+	"ori":       uint16(0x000D),
+	"xori":      uint16(0x000E),
+	"noti":      uint16(0x000F),
+	"shl":       uint16(0x0010),
+	"shr":       uint16(0x0011),
+	"inc":       uint16(0x0012),
+	"dec":       uint16(0x0013),
+	"addf":      uint16(0x0014),
+	"subf":      uint16(0x0015),
+	"mulf":      uint16(0x0016),
+	"divf":      uint16(0x0017),
+	"gef":       uint16(0x0018),
+	"lef":       uint16(0x0019),
+	"gtf":       uint16(0x001A),
+	"ltf":       uint16(0x001B),
+	"and":       uint16(0x001C),
+	"or":        uint16(0x001D),
+	"xor":       uint16(0x001E),
+	"not":       uint16(0x001F),
+	"conc":      uint16(0x0020),
+	"len":       uint16(0x0021),
+	"getc":      uint16(0x0022),
+	"setc":      uint16(0x0023),
+	"syscall":   uint16(0x0024),
+	"extern":    uint16(0x0025),
+	"call":      uint16(0xF000),
+	"jmp":       uint16(0xF001),
+	"jmpt":      uint16(0xF002),
+	"jmpf":      uint16(0xF003),
 }
 
 const (
@@ -110,12 +114,22 @@ const (
 	Variable = byte(uint8(0xF))
 )
 
+const(
+	Int8      = byte(uint8(0x8))
+	Int16     = byte(uint8(0x10))
+	Int32     = byte(uint8(0x20))
+	Int64     = byte(uint8(0x40))
+
+	Float32   = byte(uint8(0x21))
+	Float64   = byte(uint8(0x41))
+)
+
 var labels = make(map[string]uint64)
 var variables = make(map[string]uint64)
 var currentVariable = uint64(0x0)
 var currentLabel = uint64(0x0)
 
-func getUint64Btyes(val uint64) []byte {
+func getUint64Bytes(val uint64) []byte {
 	return []byte{
 		byte((val & 0xFF00000000000000) >> 56),
 		byte((val & 0x00FF000000000000) >> 48),
@@ -128,6 +142,15 @@ func getUint64Btyes(val uint64) []byte {
 	}
 }
 
+func getUint32Bytes(val uint32) []byte {
+	return []byte{
+		byte((val & 0xFF000000) >> 24),
+		byte((val & 0x00FF0000) >> 16),
+		byte((val & 0x0000FF00) >> 8),
+		byte(val & 0x000000FF),
+	}
+}
+
 func getUint16Bytes(val uint16) []byte {
 	return []byte{
 		byte((val & 0xFF00) >> 8), //Get upper 8 bits
@@ -135,11 +158,18 @@ func getUint16Bytes(val uint16) []byte {
 	}
 }
 
+func isAscii(s string) bool {
+	f := func(r rune) bool {
+		return r < 'A' || r > 'z'
+	}
+	return !(strings.IndexFunc(s, f) != -1)
+}
 
 func Compile(root types.Root, outname string) int {
 	for e := range root.Labels {
 		labels[e] = currentLabel
 		currentLabel += uint64(0x1)
+		//TODO: Add labels to beginning of file
 	}
 
 	f, err := os.Create(outname)
@@ -169,21 +199,91 @@ func Compile(root types.Root, outname string) int {
 
 		if isJmp && root.Commands[e].Param.Kind == types.Name {
 			write(f, []byte{Label})
-			write(f, getUint64Btyes(labels[root.Commands[e].Param.Text]))
+			write(f, getUint64Bytes(labels[root.Commands[e].Param.Text]))
 		} else {
 			switch root.Commands[e].Param.Kind {
 			case types.Name:
 				write(f, []byte{Variable})
 
-				if _,ok := variables[root.Commands[e].Param.Text]; !ok{
+				if _, ok := variables[root.Commands[e].Param.Text]; !ok {
 					variables[root.Commands[e].Param.Text] = currentVariable
 					currentVariable += uint64(0x1)
 				}
 
-				write(f, getUint64Btyes(variables[root.Commands[e].Param.Text]))
+				write(f, getUint64Bytes(variables[root.Commands[e].Param.Text]))
 
-			case types.HexNumber, types. Number:
+			case types.HexNumber, types.Number:
 				write(f, []byte{Int})
+				var num int64
+				num, _ = strconv.ParseInt(root.Commands[e].Param.Text, 0, 64)
+
+				if num >= -128 && num <= 127 {
+					write(f, []byte{Int8})
+					write(f, []byte{byte(int8(num))})
+				} else if num >= -32768 && num <= 32767 {
+					write(f, []byte{Int16})
+					write(f, getUint16Bytes(uint16(num)))
+				} else if num >= -2147483648 && num <= 2147483647 {
+					write(f, []byte{Int32})
+					write(f, getUint32Bytes(uint32(num)))
+				} else {
+					write(f, []byte{Int64})
+					write(f, getUint64Bytes(uint64(num)))
+				}
+
+			case types.Pointer:
+				write(f, []byte{Ptr})
+
+				ptrName := strings.Trim(strings.Trim(root.Commands[e].Param.Text,"]"),"[")
+				
+				if _, ok := variables[ptrName]; ok {
+					write(f, getUint64Bytes(variables[ptrName]))
+				}else{
+					panic("Couldn't find variable: " + ptrName + "!")
+				}
+
+			case types.String:
+				rawString := strings.Trim(root.Commands[e].Param.Text,"\"")
+
+				if isAscii(rawString) {
+					write(f, []byte{StringA})
+					stringBytes := []byte(rawString)
+					write(f, getUint64Bytes(uint64(len(stringBytes))))
+					write(f, stringBytes)
+				}else{
+					rawRunes := []rune(rawString)
+					runeBytes := []byte(string(rawRunes))
+
+					write(f, []byte{StringU})
+					write(f, getUint64Bytes(uint64(len(runeBytes))))
+					write(f, runeBytes)
+				}
+
+			//You'll float too
+			case types.Float:
+				write(f, []byte{Float})
+				switch root.Commands[e].Param.Size {
+				case "64":
+					write(f, []byte{Float64})
+
+					var num float64
+					num, _ = strconv.ParseFloat(root.Commands[e].Param.Text, 64)
+
+					var buf [8]byte
+					binary.BigEndian.PutUint64(buf[:], math.Float64bits(num))
+
+					write(f, buf[:])
+				default: //if size isn't stated, use 32 bit
+					write(f, []byte{Float32})
+
+					var num float64
+					num, _ = strconv.ParseFloat(root.Commands[e].Param.Text, 32)
+
+					var buf [4]byte
+					binary.BigEndian.PutUint32(buf[:], math.Float32bits(float32(num)))
+
+					write(f, buf[:])
+				}
 			}
 		}
 	}
